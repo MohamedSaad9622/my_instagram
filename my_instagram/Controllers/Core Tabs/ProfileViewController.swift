bba064becc3c82224834f76b857749a5fb47c0ba
mohamedSaad3 <m.saad199622019@gmail.com>
2021-12-12 18:31:41 +0200

    add collectionView to profileViewController

--- my_instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ my_instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -10,33 +10,53 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureNavigationBar()
+        private var collectionView : UICollectionView?
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            configureNavigationBar()
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            collectionView?.delegate = self
+            collectionView?.dataSource = self
+            guard let collectionView = collectionView else {
+                return
+            }
+        }
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            collectionView?.frame = view.bounds
+        }
+        func configureNavigationBar() {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
+        }
+        
+        @objc func didTapSettingsButton() {
+            let vc = SettingViewController()
+            vc.title = "Settings"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+
     }
-    
-    func configureNavigationBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingButton))
-    }
-    
-    @objc func didTapSettingButton() {
-        let vc = SettingViewController()
-        vc.title = "Settings"
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    override func viewDidAppear(_ animated: Bool) {
+
+    //MARK: - Collection view Delegate & DataSource
+
+    extension ProfileViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 0
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    //        let collection = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    //        collectionView.backgroundColor = UIColor.red
+    //        return collection
+            return UICollectionViewCell()
+        }
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            collectionView.deselectItem(at: indexPath, animated: true)
+        }
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
